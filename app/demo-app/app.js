e63e9376d42a5dcd39a30f03abad71ea9e8537c5
Ashish Mondal <mimokool2011@gmail.com>
2026-04-04 08:40:08 +0530

    fix: removed hardcoded secret and moved to env

--- app/demo-app/app.js
+++ app/demo-app/app.js
@@ -4,5 +4,5 @@ const app = express();
 app.get('/', (req, res) => {
   res.send("Hello DevSecOps");
 });
-const AWS_SECRET = "AKIA123456789SECRET";
+
 app.listen(3000, () => console.log("Running on port 3000"));
